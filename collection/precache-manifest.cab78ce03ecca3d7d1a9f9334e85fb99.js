44f815549b17c586a05d748fa72c1276c53d5b06
Aravind Sivasailam <asivasailam@esri.com>
2019-09-03 08:29:25 -0700

    add collection app

---
+++ collection/precache-manifest.cab78ce03ecca3d7d1a9f9334e85fb99.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6b5680646fb98930ccb8b99b8360d01",
+    "url": "/sm-gallery/index.html"
+  },
+  {
+    "revision": "cfb184afc19b8ecc41c3",
+    "url": "/sm-gallery/static/js/2.8b1ce101.chunk.js"
+  },
+  {
+    "revision": "d6c3e9c9bf4fefe7fdcc",
+    "url": "/sm-gallery/static/js/main.d5c85054.chunk.js"
+  },
+  {
+    "revision": "a475cb5f6c86b4ab7f02",
+    "url": "/sm-gallery/static/js/runtime~main.7a3851f7.js"
+  }
+]);
